18dbcb845a541306b70f8f6c066042902597078c
KOUSER BIBI <mechezaz@gmail.com>
2026-04-25 13:08:22 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`instrument4`, function (sprite, location) {
+    info.changeScoreBy(48)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`instrument2`, function (sprite, location) {
+    info.changeScoreBy(48)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`instrument1`, function (sprite, location) {
+    info.changeScoreBy(48)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`fan2`, function (sprite, location) {
+    info.changeScoreBy(-48)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`fan1`, function (sprite, location) {
+    info.changeScoreBy(-48)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`instrument0`, function (sprite, location) {
+    info.changeScoreBy(48)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`fan3`, function (sprite, location) {
+    info.changeScoreBy(-48)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`exit`, function (sprite, location) {
+    game.gameOver(true)
+})
+music.play(music.createSong(assets.song`Mix`), music.PlaybackMode.LoopingInBackground)
+scene.setBackgroundColor(13)
+tiles.setTilemap(tilemap`level1`)
+let mySprite = sprites.create(assets.image`rockstar`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+tiles.placeOnRandomTile(mySprite, assets.tile`stage`)
+scene.cameraFollowSprite(mySprite)
+info.startCountdown(240)
